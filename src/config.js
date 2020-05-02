93cded38815833f1dbd54952e6f8575d1067a576
johnnatha.santos <johnnatha@fullstack-service.com.br>
2020-05-01 22:07:11 -0300

    ajuste para exibir página de encerramento

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 import fetch from 'unfetch'
 
 export default {
-  apiUrl: 'https://msb.queroquero.com.br/msb/doacaoCestaBasica/v1',
+  apiUrl: 'https://hmlmsb.queroquero.com.br/msb/doacaoCestaBasica/v1',
   fetcher: (...args) => fetch(...args).then(res => res.json())
 }
